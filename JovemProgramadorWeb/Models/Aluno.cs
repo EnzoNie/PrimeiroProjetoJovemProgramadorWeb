785705798ea62605139d7416d7c41aa4f80cae96
ENZO NIETSCHE <enzo.nietsche@alunos.sc.senac.br>
2023-08-07 06:01:15 -0300

    Alterações  classe aluno

--- JovemProgramadorWeb/Models/Aluno.cs
+++ JovemProgramadorWeb/Models/Aluno.cs
@@ -2,6 +2,7 @@
 {
     public class Aluno
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Cep { get; set; }
